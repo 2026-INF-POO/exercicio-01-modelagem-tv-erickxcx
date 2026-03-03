efb4ed4c4cd7652b2e3cc843a12516c1a3de7ab5
erickxcx <20251ti0063@ifba.edu.br>
2026-03-03 09:04:15 -0300

    XCX (DEMO) Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,38 @@
+class TV{
+  int tamanho_de_tela;
+  int volume = 5;
+  int voltagem;
+  int canal;
+  String modelo;
 
+  void ligar() {
+    //lógica para a TV ligar
+}
+
+  void desligar() {
+    //lógica para a TV desligar
+  }
+
+  int aumentar_volume() {
+    if (volume < 10){
+      volume++;
+    }
+    return volume;
+  }
+
+  int diminuir_volume() {
+    if (volume > 10){
+      volume--;
+    }
+    return volume;
+  }
+
+  void subir_de_canal() {
+    // lógica para a TV subir de canal
+  }
+
+  void descer_de_canal() {
+    // lógica para a TV descer de canal
+  }
+  
+}
